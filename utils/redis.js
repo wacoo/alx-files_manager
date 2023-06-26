f3c5c57290fda9651dc3aef6dcd614a005eb3b20
wac <wabaham9@gmail.com>
2023-06-26 21:51:51 +0300

    task 0

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+/* create RedisClient class */
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isConnected = true;
+    this.client.on('error', (err) => {
+      console.log('Redis client error!', err);
+      this.isConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(str) {
+    return promisify(this.client.GET).bind(this.client)(str);
+  }
+
+  async set(key, val, duration) {
+    await promisify(this.client.SETEX).bind(this.client)(key, duration, val);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
